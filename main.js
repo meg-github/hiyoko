ce887ef1674c104795371a8d3cba5ac0f4206f05
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2014-09-16 20:06:54 +0900

    jsファイルを修正

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(function() {
 
-	$('#search').on('click', function() {
-		var query = $('#query').val();
+    $('#search').on('click', function() {
+        var query = $('#query').val();
 
-		/*if (query == "弱いひよこ") {
+        /*if (query == "弱いひよこ") {
 			$('#result').html('<img src="hiyoko_chibi.png">');
 		} else if (query == "ひよこのボス") {
 			$('#result').html('<img src="hiyoko_boss.png">');
@@ -12,13 +12,15 @@ $(function() {
 		} else if (query == "ねこ") {
 			$('#result').html('<img src="cat.png">');*/
 
-			var queryArray = ['弱いひよこ', 'ひよこのボス', 'いぬ', 'ねこ', '組長']
-			var resultArray = ['hiyoko_chibi', 'hiyoko_boss', 'dog', 'cat', 'boss']
+        /* インデントが一つズレてるよ */
 
-			for (var i = 0; i < queryArray.length; i++){
-				if (query == queryArray[i]){
-				$('#result').html('<img src="'+resultArray[i]+'.png">');	
-				}
-			}
-	});
+        var queryArray = ['弱いひよこ', 'ひよこのボス', 'いぬ', 'ねこ', '組長']; // セミコロンが抜けてるよ
+        var resultArray = ['hiyoko_chibi', 'hiyoko_boss', 'dog', 'cat', 'boss']; // セミコロンが抜けてるよ
+
+        for (var i = 0; i < queryArray.length; i++) {
+            if (query == queryArray[i]) {
+                $('#result').html('<img src="' + resultArray[i] + '.png">'); // ＋の前後にはスペースを開けよう, あとインデントがズレてるよ！
+            }
+        }
+    });
 });
